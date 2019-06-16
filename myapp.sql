aa2e8b4687ed2924589ae2bf246d5881358e4f43
motomotoyoshi <motomoto.git@gmail.com>
2019-06-16 16:45:22 +0000

    update myapp.sql, add values

--- myapp.sql
+++ myapp.sql
@@ -4,3 +4,16 @@ create table users(
   name varchar(20),
   score float
 );
+
+-- insert into users (id, name, score) values (1, 'okipo', 5.8);
+-- insert into users (id, name, score) values (2, 'myq', 4.1);
+-- insert into users (id, name, score) values (3, 'sorafa', 9.7);
+-- insert into users (id, name, score) values (4, 'aka', null);
+
+insert into users (id, name, score) values
+  (1, 'okipo', 5.8),
+  (2, 'myq', 4.1),
+  (3, 'sorafa', 9.7),
+  (4, 'aka', null);
+
+select * from users;
