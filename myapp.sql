1525e4563d19542dcb8d0352ef4ee91d77eb2904
motomotoyoshi <motomoto.git@gmail.com>
2019-06-18 13:32:55 +0000

    update myapp.sql, change table const

--- myapp.sql
+++ myapp.sql
@@ -5,12 +5,11 @@ create table users(
   -- score float not null
   score float default 0.0
 );
-desc users;
 
--- insert into users (id, name, score) values (1, 'okipo', 5.8);
--- insert into users (id, name, score) values (2, 'myq', 4.1);
--- insert into users (id, name, score) values (3, 'sorafa', 9.7);
--- insert into users (id, name, score) values (4, 'aka', null);
+alter table users add column email varchar(255) after name;
+alter table users drop column score;
+alter table users change name user_name varchar(80) default 'nobody';
+desc users;
 
 -- insert into users (id, name, score) values
 --   (1, 'okipo', 5.8),
@@ -21,9 +20,13 @@ desc users;
 -- insert into users (id, name) values (5, 'aipon');
 -- insert into users (id, name) values (6, 'aipon');
 
-insert into users (name, score) values ('okipo', 5.8);
-insert into users (name, score) values ('myq', 4.1);
-insert into users (name, score) values ('sorafa', 9.7);
-insert into users (name, score) values ('aka', 3.3);
+-- insert into users (name, score) values ('okipo', 5.8);
+-- insert into users (name, score) values ('myq', 4.1);
+-- insert into users (name, score) values ('sorafa', 9.7);
+-- insert into users (name) values ('aka');
 
 select * from users;
+
+alter table users rename persons;
+show tables;
+drop table if exists persons;
