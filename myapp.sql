4346aa224846b02c7c7a94bc8c6c6b624a99a446
motomotoyoshi <motomoto.git@gmail.com>
2019-06-18 13:17:15 +0000

    update myapp.sql

--- myapp.sql
+++ myapp.sql
@@ -1,19 +1,29 @@
 drop table if exists users;
 create table users(
-  id int unsigned,
-  name varchar(20),
-  score float
+  id int unsigned primary key auto_increment,
+  name varchar(20) unique,
+  -- score float not null
+  score float default 0.0
 );
+desc users;
 
 -- insert into users (id, name, score) values (1, 'okipo', 5.8);
 -- insert into users (id, name, score) values (2, 'myq', 4.1);
 -- insert into users (id, name, score) values (3, 'sorafa', 9.7);
 -- insert into users (id, name, score) values (4, 'aka', null);
 
-insert into users (id, name, score) values
-  (1, 'okipo', 5.8),
-  (2, 'myq', 4.1),
-  (3, 'sorafa', 9.7),
-  (4, 'aka', null);
+-- insert into users (id, name, score) values
+--   (1, 'okipo', 5.8),
+--   (2, 'myq', 4.1),
+--   (3, 'sorafa', 9.7);
+--   -- (4, 'aka', null);
+-- 
+-- insert into users (id, name) values (5, 'aipon');
+-- insert into users (id, name) values (6, 'aipon');
+
+insert into users (name, score) values ('okipo', 5.8);
+insert into users (name, score) values ('myq', 4.1);
+insert into users (name, score) values ('sorafa', 9.7);
+insert into users (name, score) values ('aka', 3.3);
 
 select * from users;
