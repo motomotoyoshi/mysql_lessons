2eb95a8a71595adb7207a16366d325b489c27d19
motomotoyoshi <motomoto.git@gmail.com>
2019-06-19 02:10:00 +0000

    update myapp, enum

--- myapp.sql
+++ myapp.sql
@@ -2,7 +2,8 @@ drop table if exists users;
 create table users(
   id int unsigned primary key auto_increment,
   name varchar(20) unique,
-  score float default 0.0
+  score float default 0.0,
+  rank enum('gold', 'silver', 'bronze')
 );
 
 desc users;
@@ -13,44 +14,10 @@ desc users;
 --   (3, 'sorafa', 9.7),
 --   (4, 'aka', null);
 -- 
-insert into users (name, score) values ('okipo', 5.8);
-insert into users (name, score) values ('myq', 4.1);
-insert into users (name, score) values ('sorafa', 9.7);
-insert into users (name, score) values ('aka', null);
-insert into users (name, score) values ('aipon', 8.0);
-insert into users (name, score) values ('imo', 1.1);
-insert into users (name, score) values ('seto', 7.1);
+insert into users (name, score, rank) values ('okipo', 5.8, 'gold');
+insert into users (name, score, rank) values ('myq', 4.1, 'bronze');
+insert into users (name, score, rank) values ('sorafa', 9.7, 'silver');
+insert into users (name, score, rank) values ('aka', null, 'blue');
 
-select length('hello');
-select substr('hello', 2);drop table if exists users;
-create table users(
-  id int unsigned primary key auto_increment,
-  name varchar(20) unique,
-  score float default 0.0
-);
-
-desc users;
-
--- insert into users (id, name, score) values
---   (1, 'okipo', 5.8),
---   (2, 'myq', 4.1),
---   (3, 'sorafa', 9.7),
---   (4, 'aka', null);
--- 
-insert into users (name, score) values ('okipo', 5.8);
-insert into users (name, score) values ('myq', 4.1);
-insert into users (name, score) values ('sorafa', 9.7);
-insert into users (name, score) values ('aka', null);
-insert into users (name, score) values ('aipon', 8.0);
-insert into users (name, score) values ('imo', 1.1);
-insert into users (name, score) values ('seto', 7.1);
-
--- select length('hello');
--- select substr('hello', 2);
--- select substr('hello', 2, 3);
--- select upper('hello');
--- select lower('hello');
--- select concat('hello', 'world');
-
--- select length(name), name from users order by length(name);
-select length(name) as len, name from users order by len;
+-- select * from users;
+select * from users where rank = 2;
