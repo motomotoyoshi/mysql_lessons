a2cc4c422e8d6486cb7a4b1b62d62818854b4883
motomotoyoshi <motomoto.git@gmail.com>
2019-06-19 02:38:32 +0000

    update myapp, set

--- myapp.sql
+++ myapp.sql
@@ -3,7 +3,7 @@ create table users(
   id int unsigned primary key auto_increment,
   name varchar(20) unique,
   score float default 0.0,
-  rank enum('gold', 'silver', 'bronze')
+  coins set('gold', 'silver', 'bronze')
 );
 
 desc users;
@@ -14,10 +14,12 @@ desc users;
 --   (3, 'sorafa', 9.7),
 --   (4, 'aka', null);
 -- 
-insert into users (name, score, rank) values ('okipo', 5.8, 'gold');
-insert into users (name, score, rank) values ('myq', 4.1, 'bronze');
-insert into users (name, score, rank) values ('sorafa', 9.7, 'silver');
-insert into users (name, score, rank) values ('aka', null, 'blue');
+insert into users (name, score, coins) values ('okipo', 5.8, 'gold,silver');
+insert into users (name, score, coins) values ('myq', 4.1, 'bronze,bronze');
+insert into users (name, score, coins) values ('sorafa', 9.7, 'silver,gold');
+insert into users (name, score, coins) values ('aka', null, 'blue');
 
 -- select * from users;
-select * from users where rank = 2;
+-- select * from users where coins = 'gold,silver';
+-- select * from users where coins like '%gold%';
+select * from users where coins = 3;
