fbc504dfa3ae8eeee03c2c9d49be3660eaed11d6
motomotoyoshi <motomoto.git@gmail.com>
2019-06-22 15:59:18 +0000

    foreign key constraint

--- myapp.sql
+++ myapp.sql
@@ -7,9 +7,11 @@ create table posts (
 drop table if exists comments;
 create table comments (
   id int unsigned primary key auto_increment,
-  post_id int not null,
+  post_id int unsigned not null,
   body text
 );
+
+alter table comments add constraint fk_comments foreign key (post_id) references posts (id);
 
 insert into posts (title, body) values ('title 1', 'body 1');
 insert into posts (title, body) values ('title 2', 'body 2');
@@ -20,7 +22,5 @@ insert into comments (post_id, body) values (1, 'second comments for post 1');
 insert into comments (post_id, body) values (3, 'first comments for post 3');
 insert into comments (post_id, body) values (4, 'first comments for post 4');
 
--- select * from posts left outer join comments on posts.id = comments.post_id;
--- select * from posts right outer join comments on posts.id = comments.post_id;
--- select * from posts left outer join comments on posts.id = comments.post_id;
-select * from posts right outer join comments on posts.id = comments.post_id;
+select * from posts;
+select * from comments;
