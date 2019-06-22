bb943b5f50f699de01e4cf68d9bdc958871432d4
motomotoyoshi <motomoto.git@gmail.com>
2019-06-22 16:34:09 +0000

    create trigger

--- myapp.sql
+++ myapp.sql
@@ -4,25 +4,21 @@ create table posts (
   title varchar(255),
   body text
 );
-drop table if exists comments;
-create table comments (
+drop table if exists logs;
+create table logs (
   id int unsigned primary key auto_increment,
-  post_id int unsigned not null,
-  body text
+  msg varchar(255)
 );
+
+drop trigger if exists posts_insert_trigger;
+create trigger posts_insert_trigger after insert on posts for each row insert into logs (msg) values ('post added!');
 
 insert into posts (title, body) values ('title 1', 'body 1');
 insert into posts (title, body) values ('title 2', 'body 2');
 insert into posts (title, body) values ('title 3', 'body 3');
 
-insert into comments (post_id, body) values (1, 'first comments for post 1');
-insert into comments (post_id, body) values (1, 'second comments for post 1');
-insert into comments (post_id, body) values (3, 'first comments for post 3');
-
-delete from posts where id = 2;
-insert into posts (title, body) values ('new title', 'new body');
--- insert into comments (post_id, body) values (4, 'first comments for new post');
-insert into comments (post_id, body) values (last_insert_id(), 'first comments for new post');
-
 select * from posts;
-select * from comments;
+select * from logs;
+
+-- show triggers;
+show triggers \G;
